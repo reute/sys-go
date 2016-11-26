0dc34038349ed3440ba86320c6324188a0ed5b3b
reute <j.rtl@gmx.de>
2016-11-26 16:44:53 +0100

    .

--- 13/dreikreuze/dreikreuze.go
+++ 13/dreikreuze/dreikreuze.go
@@ -4,19 +4,84 @@ import (
 	"fmt"
 	"os"
 	"math/rand"
+    "time"
 )
 
-var spielfeld, letzterZug uint
-
-const WORD_BIT = 23
+const spielfeldbreite = 23
 const (
-	Spieler = iota
-	Computer
+	spieler = iota
+	computer
 )
+var amZug = computer
+var spielfeld uint
+
+func main() {
+	rand.Seed(time.Now().UTC().UnixNano())
+	fmt.Println("*** Drei Kreuze ***\nGegeben ist eine Kette von 23 freien Feldern. In jedem Zug setzt jeder der Spieler ein X auf ein freies Feld. Wenn dadurch drei oder mehr X benachbart sind, hat der Spieler gewonnen.")
+	zeichneSpielfeld()
+	var anfangen string
+	fmt.Print("Wollen Sie anfangen? (j/n):  ")
+	fmt.Scanf("%s", &anfangen)
+	if anfangen == "j" {
+		amZug = spieler
+	} 
+	gameloop()
+	exit()
+}
+
+func gameloop() {
+	for {
+		neuerZug()
+        zeichneSpielfeld()
+		if pruefeSpielstand() != true {
+			break
+		}	 
+		amZug = (amZug + 1) % 2
+	}
+}
+
+func exit() {
+	if amZug == spieler {
+		fmt.Println("Der Spieler gewinnt !")
+	} else {
+		fmt.Println("Der Computer gewinnt !")
+	}
+}
+
+func neuerZug() {
+	var zug uint
+	if amZug == computer {		
+		for {
+			zug  = uint(rand.Intn(int(spielfeldbreite)))
+			if feldBelegen(zug) {
+				break
+			}
+		}
+		fmt.Printf("Der Computer belegt das Feld %d\n", zug)
+	} else {		
+		for {
+			fmt.Print("Ihr Zug : ")
+			fmt.Scanf("%d", &zug)
+			if zug == 99 {
+				fmt.Println("Program wird beendet !")
+				os.Exit(0)
+			}
+			if zug < 0 || zug > spielfeldbreite {
+				fmt.Printf("Bitte Zahl zwischen 0 und %d eingeben\n", spielfeldbreite-1)
+				continue
+			}	
+			if feldBelegen(zug) {
+				break
+			} else {
+				fmt.Println("Feld ist schon belegt !")  
+			}
+		}
+	}
+}
 
 func zeichneSpielfeld() {
 	var i uint
-	for i = 0; i < WORD_BIT; i++ {
+	for i = 0; i < spielfeldbreite; i++ {
 		if feldBelegt(i) {
 			fmt.Print("\\/ ")
 		} else {
@@ -25,7 +90,7 @@ func zeichneSpielfeld() {
 	}
 	fmt.Println()
 
-	for i = 0; i < WORD_BIT; i++ {
+	for i = 0; i < spielfeldbreite; i++ {
 		if feldBelegt(i) {
 			fmt.Print("/\\ ")
 		} else {
@@ -34,7 +99,7 @@ func zeichneSpielfeld() {
 	}
 	fmt.Println()
 
-	for i = 0; i < WORD_BIT; i++ {
+	for i = 0; i < spielfeldbreite; i++ {
 		fmt.Printf("%02d ", i)
 	}
 	fmt.Println()
@@ -60,81 +125,18 @@ func feldBelegt(pos uint) (bool) {
 	}
 }
 
-func zugSpieler() {
-	letzterZug = Spieler
-	var zug uint	
-	for {
-		fmt.Print("Ihr Zug : ")
-		fmt.Scanf("%d", &zug)
-		if zug == 99 {
-			fmt.Println("Program wird beendet !")
-			os.Exit(0)
-		}
-
-		if zug < 0 || zug > WORD_BIT {
-			 fmt.Printf("Bitte Zahl zwischen 0 und %d eingeben\n", WORD_BIT-1)
-			 continue
-		}	
-		if feldBelegen(zug) {
-            break
-        } else {
-            fmt.Println("Feld ist schon belegt !")  
-        }
-	}	
-}
-
-func zugComputer() {
-	letzterZug = Computer
-	var pos uint	
-	for {
-		pos  = uint(rand.Intn(int(WORD_BIT)))
-		if feldBelegen(pos) {
-			break
-		}
-	}
-	fmt.Printf("Der Computer belegt das Feld %d\n", pos)
-}
-
-func pruefeSpielstand() {
+func pruefeSpielstand() bool {
 	var kreuze uint;
 	var i uint;
-	for i = 0; i < WORD_BIT; i++ {
+	for i = 0; i < spielfeldbreite; i++ {
     	if feldBelegt(i) {
 			kreuze++
     	} else {
         	kreuze = 0    
    		} 
    		if kreuze == 3 {
-   			if letzterZug == Spieler {
-   				fmt.Println("Der Spieler gewinnt !")
-   			} else {
-				fmt.Println("Der Computer gewinnt !")
-   			}
-   			os.Exit(0)
+			return false   			
    		}   	
 	}
-}
-
-func main() {
-	var anfangen string
-	rand.Seed(42)
-
-	fmt.Println("*** Drei Kreuze ***\nGegeben ist eine Kette von 23 freien Feldern. In jedem Zug setzt jeder der Spieler ein X auf ein freies Feld. Wenn dadurch drei oder mehr X benachbart sind, hat der Spieler gewonnen.")
-	zeichneSpielfeld()
-	fmt.Print("Wollen Sie anfangen? (j/n):  ")
-	fmt.Scanf("%s", &anfangen)
-
-	if anfangen == "j" {
-        zugSpieler()
-        zeichneSpielfeld()
-        pruefeSpielstand()
-	} 
-	for {
-        zugComputer()
-        zeichneSpielfeld()
-        pruefeSpielstand()
-        zugSpieler()
-        zeichneSpielfeld()
-        pruefeSpielstand()
-	}
+	return true
 }
