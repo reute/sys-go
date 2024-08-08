c578ec562a7cec957e1447141ffa96c4c11041d8
jreutlinger <j.rtl@outlook.de>
2024-08-08 22:02:51 +0200

    fcalc

--- 13/fcalc/fcalc.go
+++ 13/fcalc/fcalc.go
@@ -1,84 +1,71 @@
 package main
 
-import ("fmt";"os"; "bufio"; "strings"; "strconv"; "math")
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
 
-type stack []float64
+type stack []float32
 
-func (s stack) Peek() float64   {
-     return s[len(s)-1] 
+func (s stack) Peek() float32 {
+	return s[len(s)-1]
 }
 
-func (s *stack) Put(f float64)  {
-     *s = append(*s, f) 
+func (s *stack) Put(f float32) {
+	*s = append(*s, f)
 }
 
-func (s *stack) Pop() float64 {
+func (s *stack) Pop() float32 {
 	f := (*s)[len(*s)-1]
 	*s = (*s)[:len(*s)-1]
 	return f
 }
 
-func main() {    
-    fmt.Print("Function: ")
-    // function := inputFunction()  
-    function := "1 2 +"
-    result := calculate(strings.Fields(function))
-    fmt.Printf("Result : %f\n", result)
+func main() {
+	fmt.Print("Function: ")
+	function := inputFunction()
+	//function := "6 3 + 1 -"
+	result := calculate(strings.Fields(function))
+	fmt.Printf("Result : %f\n", result)
 }
 
-func calculate(function []string) float64 {
-    var ops stack
-    vars := make(map[string]float64)
-    for _, e := range(function) {
-        f, err := strconv.ParseFloat(e, 64)        
-        if err == nil {
-            ops.Put(f)
-        } else {            
-            switch e {
-                case "+" : 
-                    op1 := ops.Pop()
-                    op2 := ops.Pop() 
-                    ops.Put(op1 + op2)
-                case "-" :
-                    op1 := ops.Pop()
-                    op2 := ops.Pop() 
-                    ops.Put(op1 - op2)
-                case "*" :
-                    op1 := ops.Pop()
-                    op2 := ops.Pop() 
-                    ops.Put(op1 * op2)
-                case "/" :
-                    op1 := ops.Pop()
-                    op2 := ops.Pop() 
-                    ops.Put(op2 / op1)
-                case "sin" :
-                    op1 := ops.Pop()
-                    ops.Put(math.Sin(op1))
-                case "cos" :
-                    op1 := ops.Pop()
-                    ops.Put(math.Cos(op1))
-                case "tan":
-                    op1 := ops.Pop()
-                    ops.Put(math.Tan(op1)) 
-                default:
-                    var num float64
-                    val, err := vars[e]
-                    if err == true {
-                        ops.Put(val) 
-                    } else {
-                        fmt.Printf("Please enter value for variable %s : ", e)
-                        fmt.Scanf("%f", &num)
-                        vars[e] = num
-                        ops.Put(num)  
-                    }
-           } 
-        }
-    }
-    return ops.Pop()
+func calculate(function []string) float32 {
+	var stack stack
+	for _, token := range function {
+		switch token {
+		case "+":
+			op2 := stack.Pop()
+			op1 := stack.Pop()
+			stack.Put(op1 + op2)
+		case "-":
+			op2 := stack.Pop()
+			op1 := stack.Pop()
+			stack.Put(op1 - op2)
+		case "*":
+			op2 := stack.Pop()
+			op1 := stack.Pop()
+			stack.Put(op1 * op2)
+		case "/":
+			op2 := stack.Pop()
+			op1 := stack.Pop()
+			stack.Put(op1 / op2)
+		default:
+			num, err := strconv.ParseFloat(token, 32)
+			if err == nil {
+				stack.Put(float32(num))
+			} else {
+				fmt.Printf("Invalid token: %s\n", token)
+			}
+		}
+	}
+	return stack.Pop()
 }
 
-func inputFunction() []string {
-    in := bufio.NewReader(os.Stdin)
+func inputFunction() string {
+	in := bufio.NewReader(os.Stdin)
 	str, _ := in.ReadString('\n')
-    return strings.Fields(str)
+	return str
 }
