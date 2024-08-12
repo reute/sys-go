f9d03cbdebabe6bf7a7c933f0297b2ac8169d1ae
jreutlinger <j.rtl@outlook.de>
2024-08-12 08:42:46 +0200

    sorthtings

--- 13/sortthings/sortthings.go
+++ 13/sortthings/sortthings.go
@@ -1,102 +1,115 @@
 package main
 
-import ("fmt"; "os"; "bufio"; "math/rand"; "strings"; "strconv") 	
+import (
+	"bufio"
+	"fmt"
+	"math/rand"
+	"os"
+	"strconv"
+	"strings"
+)
 
-const MTN_MAX = 8
+const MAX_MOUNTAINS = 8
 const FILENAME = "berge"
 
 type mountain struct {
-    name string
-    height int
+	name   string
+	height int
+}
+
+type mountains struct {
+	unsorted []*mountain
+	sorted   []*mountain
 }
 
 func main() {
-    var from, to int  
-    rand.Seed(42)     
-    taken := make([]*mountain, 0, MTN_MAX)
-    left := make([]*mountain, MTN_MAX)
-    arrayMountains := readFile()
-    for i := 0; i < len(arrayMountains); i++ {
-        left[i] = &arrayMountains[i]
-    }  
-    // gameloop
-    for isSorted(taken) && len(left) != 0  {
-        fmt.Println("Current state:")
-        printMountains(taken)
-        fmt.Println("Still to be sorted:")
-        printMountains(left)
-        from, to = inputUser()
-        taken, left = move(from, left, to, taken)   
-    }
-    if len(left) == 0 {        
-       fmt.Println("You have  won !!")  
-    } else {
-       fmt.Println("Sorry, then it is no longer sorted:")       
-    }
-    for _, element := range(taken) {
-        fmt.Printf(" %5d: %s \n", (*element).height, (*element).name)
-    }
-    fmt.Println("Bye!")
-    fmt.Printf("You got %d Points.\n", len(taken) - 1) 
+	var from, to int
+	all_mountains := readFile()
+	fmt.Println(all_mountains)
+	game := mountains{
+		unsorted: make([]*mountain, len(all_mountains)),
+		sorted:   make([]*mountain, 0),
+	}
+	for i := 0; i < len(all_mountains); i++ {
+		game.unsorted[i] = &all_mountains[i]
+	}
+	// gameloop
+	for isSorted(game.sorted) && len(game.unsorted) != 0 {
+		fmt.Println("Sorted Mountains:")
+		printMountains(game.sorted)
+		fmt.Println("Still to be sorted:")
+		printMountains(game.unsorted)
+		from, to = inputUser()
+		game.move(from, to)
+	}
+	if len(game.unsorted) == 0 {
+		fmt.Println("You have  won !!")
+	} else {
+		fmt.Println("Sorry, then it is no longer sorted:")
+	}
+	for _, element := range game.sorted {
+		fmt.Printf(" %5d: %s \n", (*element).height, (*element).name)
+	}
+	fmt.Println("Bye!")
+	fmt.Printf("You got %d Points.\n", len(game.sorted)-1)
 }
 
 func isSorted(mountains []*mountain) bool {
-    var tmp, hightest int
-    for _, element := range(mountains) {
-        tmp = (*element).height
-        if tmp > hightest {
-            hightest = tmp
-        } else {
-            return false
-        }
-    }
-    return true
+	var tmp, hightest int
+	for _, element := range mountains {
+		tmp = (*element).height
+		if tmp > hightest {
+			hightest = tmp
+		} else {
+			return false
+		}
+	}
+	return true
 }
 
 func inputUser() (int, int) {
-    fmt.Println("What is to be inserted where? ")
-    in := bufio.NewReader(os.Stdin)
+	fmt.Println("What is to be inserted where? ")
+	in := bufio.NewReader(os.Stdin)
 	str, _ := in.ReadString('\n')
-    strSlice := strings.Fields(str)
-    from, _ := strconv.Atoi(strSlice[0])
-    to, _ := strconv.Atoi(strSlice[1])  
-    return from, to  
+	strSlice := strings.Fields(str)
+	from, _ := strconv.Atoi(strSlice[0])
+	to, _ := strconv.Atoi(strSlice[1])
+	return from, to
 }
 
-func readFile() (arrayMountains [MTN_MAX]mountain) {   
-    inFile, _ := os.Open(FILENAME)
-    defer inFile.Close()
-    scanner := bufio.NewScanner(inFile)
-	scanner.Split(bufio.ScanLines) 
-    var tmp []string
-    var name string
-    var height, r int
-    for i := 0; scanner.Scan(); i++ {
-        tmp = strings.Split(scanner.Text(), ":")
-        name = tmp[0]
-        height, _ = strconv.Atoi(tmp[1])         
-        if i < MTN_MAX {
-           arrayMountains[i] = mountain{name, height}
-        } else {
-            r = rand.Intn(i)
-            if r < MTN_MAX {
-                arrayMountains[r] = mountain{name, height}
-            }
-        }
-    }
-    return
+func readFile() (mountains [MAX_MOUNTAINS]mountain) {
+	inFile, _ := os.Open(FILENAME)
+	defer inFile.Close()
+	scanner := bufio.NewScanner(inFile)
+	scanner.Split(bufio.ScanLines)
+	var tmp []string
+	var name string
+	var height, r int
+	for i := 0; scanner.Scan(); i++ {
+		tmp = strings.Split(scanner.Text(), ":")
+		name = tmp[0]
+		height, _ = strconv.Atoi(tmp[1])
+		if i < MAX_MOUNTAINS {
+			mountains[i] = mountain{name, height}
+		} else {
+			r = rand.Intn(i)
+			if r < MAX_MOUNTAINS {
+				mountains[r] = mountain{name, height}
+			}
+		}
+	}
+	return
 }
 
-func move(from int, left []*mountain, to int, taken []*mountain) ([]*mountain, []*mountain) {
-    taken = append(taken, &mountain{})
-    copy(taken[to+1:], taken[to:])
-    taken[to] = left[from]
-    left = append(left[:from], left[from+1:]...)    
-    return taken, left
+func (m *mountains) move(from int, to int) {
+	m.sorted = append(m.sorted, &mountain{})
+	copy(m.sorted[to+1:], m.sorted[to:])
+	m.sorted[to] = m.unsorted[from]
+	m.unsorted = append(m.unsorted[:from], m.unsorted[from+1:]...)
 }
 
 func printMountains(mountains []*mountain) {
-    for index, element := range(mountains) {
-        fmt.Printf(" %d: %s\n", index, (*element).name)
-    }
+	for i, element := range mountains {
+		fmt.Printf(" %d: %s\n", i, (*element).name)
+	}
 }
